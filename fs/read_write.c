641c5373381b704d13683941b7998f744c5680ff
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-06 21:14:10 +0300

    fs: Add APIs to block specific userspace services from running
    Unfortunately, there are several bloatware services that do not cope well
    with their kernel interfaces disappearing. Furthermore, there are other
    services that are nasty for different reasons and should be disabled.
    
    But there's no way to disable them from the kernel without ungodly VFS
    hacks, so that's where we're left: killing poison with poison.
    
    Tashar02: This is a trimmed version of the original patch, which only
    includes the service blocker and VINTF removal hack.
    
    Original patch: https://github.com/kerneltoast/android_kernel_google_gs201/commit/d83a2cdefc7b53
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -573,6 +573,20 @@ static inline loff_t *file_ppos(struct file *file)
 	return file->f_mode & FMODE_STREAM ? NULL : &file->f_pos;
 }
 
+bool task_is_servicemanager(struct task_struct *p);
+static void remove_vintf(char __user *buf, size_t len)
+{
+	static const char *const vintf;
+	char __user *start;
+
+	/* Replace the VINTF XML entry with whitespace */
+	uaccess_enable_privileged();
+	start = strnstr(buf, vintf, len);
+	if (unlikely(start))
+		memset(start, ' ', strlen(vintf));
+	uaccess_disable_privileged();
+}
+
 ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 {
 	struct fd f = fdget_pos(fd);
@@ -585,8 +599,11 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 			ppos = &pos;
 		}
 		ret = vfs_read(f.file, buf, count, ppos);
-		if (ret >= 0 && ppos)
-			f.file->f_pos = pos;
+		if (ret >= 0) {
+			remove_vintf(buf, ret);
+			if (ppos)
+				f.file->f_pos = pos;
+		}
 		fdput_pos(f);
 	}
 	return ret;
